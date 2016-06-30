95d17601897c80fae65d189e1400e3ab63230738
Ryan Lovett <rylo@berkeley.edu>
2016-06-29 22:47:25 -0700

    PUBLIC_IPS is not seeded until public_ips() is run

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -2,6 +2,6 @@ c.NotebookApp.open_browser = False
 # The IP address the notebook server will listen on.
 try:
     import jupyter_client
-    c.NotebookApp.ip = jupyter_client.localinterfaces.PUBLIC_IPS[0]
+    c.NotebookApp.ip = jupyter_client.localinterfaces.public_ips()[0]
 except:
     c.NotebookApp.ip = 'localhost'
